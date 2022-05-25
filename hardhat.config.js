2188ee5e91530655e33b5a7cd2e90db68b4444c7
AbuzarGhazanfarKhan <abuzarghaznafrkhanagk@gmail.com>
2022-05-26 03:07:12 +0500

    Bugs & NFT Market Contract Update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 const fs=require("fs")
-const privateKey=fs.readFileSync(".secret").toString()
+const privateKey = fs.readFileSync(".secret").toString()
+console.log(privateKey)
 
 module.exports = {
   networks:{
@@ -9,9 +10,8 @@ module.exports = {
     },
     mumbai:{
       url:"https://polygon-mumbai.g.alchemy.com/v2/O9HlXwFBx9fkCD8YSw3yN2wlIyKqCNUB",
-      accounts:[privateKey]
-    },
-    mainnet:{}
+      accounts:[`0x${privateKey}`]
+    }
   },
   solidity: "0.8.4",
 };
